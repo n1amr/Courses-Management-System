a8234dc81115f30b0b049124a869e1a6668fd662
ahmedroshdy <ahmedroshdy2013@hotmail.com>
2016-05-03 02:07:12 +0200

    Template<BST>

---
+++ CoursesManagementSystem/BST.h
@@ -0,0 +1,26 @@
+#ifndef BinarySearchTree_H_
+#define BinarySearchTree_H_
+#include"TreeNode.h"
+template <class T>
+class BST
+{
+public:
+	BST();
+	BST(TreeNode<T> *root );
+	TreeNode<T>* Search(T d);
+	bool Insert(T d);
+	void Remove(T d);
+	TreeNode<T>* getRoot();
+	TreeNode<T>* getRightSubtree();
+	TreeNode<T>* getLeftSubtree();
+	bool isEmpty();
+	bool isLeaf(TreeNode<T>* t);
+	void PrintInorder();
+	
+private:
+	TreeNode<T> *Root;
+	void PrintInorderhelper(TreeNode<T>* ptr);
+};
+
+#include"BST.cpp"
+#endif
---
+++ CoursesManagementSystem/TreeNode.cpp
@@ -0,0 +1,36 @@
+#ifndef TreeNode_cpp_
+#define TreeNode_cpp_
+#include"TreeNode.h"
+
+template <class T>
+TreeNode<T>::TreeNode(T x,TreeNode<T> *left, TreeNode<T> *right){
+	Data = x;
+	this->Left = left;
+	this->Right = right;
+}
+template <class T>
+T TreeNode<T>::GetData(){
+	return Data;
+}
+template <class T>
+TreeNode<T>* TreeNode<T>::GetLeft(){
+	return Left;
+}
+template <class T>
+TreeNode<T>* TreeNode<T>::GetRight(){
+	return Right;
+}
+template <class T>
+void TreeNode<T>::SetData(T d){
+	Data = d;
+}
+template <class T>
+void TreeNode<T>::SetLeft(TreeNode<T>* left){
+	Left = left;
+}
+template <class T>
+void TreeNode<T>::SetRight(TreeNode<T>* right){
+	Right = right;
+}
+
+#endif
